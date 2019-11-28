91add0818d03601a1fff3e9335f0683fe074104f
System Administrator <root@dgalloway-2.local>
2019-11-28 12:42:45 -0500

    Test

--- app/move.rb
+++ app/move.rb
@@ -2,14 +2,14 @@
 # View docs at https://docs.battlesnake.com/snake-api for example payloads.
 def move(board)
   puts "Hello, logs!"
-  occupied_spaces = []
+#  occupied_spaces = []
+#  pp board
+#  pp board{:turn}
+#  board[:snakes].each do |snake|
+#  	snake[:body].each do |body_part|
+#  		pp body_part
+#  	end
+#  end
   pp board
-  pp board{:turn}
-  board[:snakes].each do |snake|
-  	snake[:body].each do |body_part|
-  		pp body_part
-  	end
-  end
-  #pp board
   { move: :left }
 end
